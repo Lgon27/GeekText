148b4c9059f688eca20bb824e942cf177bd39071
Sebastian <sgarc349@fiu.edu>
2019-10-15 17:16:34 -0400

    book adding

---
+++ geektext-server/models/books.js
@@ -0,0 +1,43 @@
+//Creates the database schema for a user
+const mongoose = require('mongoose')
+
+
+/*
+DESCRIPTION: Stores Book information
+*/
+const bookSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    cover_image: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    publish_date: {
+        type: String,
+        required: true
+    },
+    author_bio: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('books', bookSchema)
--- geektext-server/routes/posts.js
+++ geektext-server/routes/posts.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 const userSchema = require('../models/users')
+const bookSchema = require('../models/books')
 
 router.post('/user', (req, res) => {
     const user = new userSchema({
@@ -24,7 +25,24 @@ router.post('/user', (req, res) => {
     }).catch(err => {
         res.json({ message: err });
     })
-
+})
+router.post('/books', (req, res) => {
+    const book = new bookSchema({
+            title: req.body.title,
+            cover_image: req.body.cover_image,
+            summary: req.body.summary,
+            genre: req.body.genre,
+            author: req.body.author,
+            price: req.body.price,
+            publish_date: req.body.publish_date,
+            author_bio: req.body.author_bio
+        })
+    
+        book.save().then(data => {
+            res.json(data);
+        }).catch(err => {
+            res.json({ message: err });
+        })
 
 })
 
