88cac58f28ea20128ed7eb6ca097ea90f4f34798
Sebastian <sgarc349@fiu.edu>
2019-10-26 17:27:59 -0400

    dynamic loading of books finally done

--- book-store/src/components/Home.js
+++ book-store/src/components/Home.js
@@ -1,71 +1,64 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { addToCart } from './actions/cartActions'
 import {Button} from 'react-bootstrap';
 
+
 class Home extends Component{
-    
+
     constructor(props){
-        super(props);
-        this.state = {books:[] }
+        super(props)
+        this.state = {
+        books: [] //initialize books as empty array
+        }
     }
 
-    handleClick = (id)=>{
-        this.props.addToCart(id); 
-    }
-    async componentDidMount() {
-        const response = await fetch('http://localhost:3000/get/books');
-        const json = await response.json();
-        console.log(json);
-    }
-
+    componentDidMount() {
+        fetch('http://localhost:3000/get/books')
+          .then(res => res.json())
+          .then(books => {
+            console.log(books);
+            this.setState({ books}); // Notify your component that products have been fetched
+          })
+          console.log(this.state.books);
+          
+      }
+    
     render(){
-
-        let itemList = this.props.items.map(item=>{
-            return(
-                <div className="card" key={item.id}>
-                        <div className="card-image">
-                                <Button id='book-button' onClick={() => this.setState({
-                                    bookTitle: item.title,
-                                    bookDetailsShow:true
-                                    })}>
-                                <img src={item.img} alt={item.title}/>
-                                </Button>
-
-                            <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" 
-                                onClick={()=>{this.handleClick(item.id)}}><i className="material-icons">add</i>
-                            </span>
-                        </div>
-
-                        <div className="card-content">
-                            <p>{item.desc}</p>
-                            <p><b>Price: ${item.price}</b></p>
-                        </div>
-                 </div>
-            )
-        })
+        const books = this.state.books;
 
         return(
             <div className="container">
                 <h3 className="center">BOOKS</h3>
+                <h2> sort by <button > author </button></h2>
                 <div className="box">
-                    {itemList}
-                </div>
+            
+                {books.map( book =>  ( 
+                    <div className="card" key={book._id}>
+                    <div className="card-image">
+                            <Button id='book-button'>
+                            <img src={book.cover_image} alt={book.title}/>
+                            </Button>
+
+                        <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" 
+                            //onClick={()=>{this.handleClick(book.id)}}
+                            ><i className="material-icons">add</i>
+                        </span>
+                    </div>
+
+                    <div className="card-content">
+                        <p>{book.author}</p>
+                        <p><b>Price: ${book.price}</b></p>
+                    </div>
+                    </div>
+                    ))}
             </div>
-        )
+            </div>
+               
+            );  
+        }
+        
+          
     }
-
-}
-const mapStateToProps = (state)=>{
-    return {
-      items: state.items
-    }
-  }
-const mapDispatchToProps = (dispatch)=>{
     
-    return{
-        addToCart: (id)=>{dispatch(addToCart(id))}
-    }
-}
 
-export default connect(mapStateToProps,mapDispatchToProps)(Home)
+
+export default Home;
