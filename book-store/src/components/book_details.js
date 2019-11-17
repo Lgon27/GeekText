8d9865350f37c779733eeb76fd363948a0cf4429
Andy6x <kwx398@hotmail.com>
2019-11-16 23:12:12 -0500

    update

---
+++ book-store/src/components/book_details.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./book_style.css";
+import book from "./book.png";
+import rating from "./rating.png";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
+export class book_details extends Component {}
+
+export default book_details;
